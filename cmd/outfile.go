643ad58e18abd8c57825d90552922658e150e779
zonewave <ybzhang17@163.com>
2023-06-05 21:07:18 +0800

    tt

--- cmd/outfile.go
+++ cmd/outfile.go
@@ -1,8 +1,13 @@
 package cmd
 
 import (
+	"os"
+
+	"github.com/cockroachdb/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/zonewave/copyer/common"
+	"github.com/zonewave/copyer/generate"
 )
 
 var outfileCmd = &cobra.Command{
@@ -16,9 +21,48 @@ var outfileCmd = &cobra.Command{
 			logrus.Errorf("RootCmdFlagGet error:%v", err)
 			return
 		}
-
+		env, err := NewEnv()
+		if err != nil {
+			logrus.Errorf("Env error:%v", err)
+			return
+		}
+		err = OutFileCopy(cmdFlag, env)
+		if err != nil {
+			logrus.Errorf("copyer error:%+v", err)
+		}
 		logrus.WithFields(logrus.Fields{
 			"cmdFlag": cmdFlag,
+			"env":     env,
 		}).Info("copyer is end...")
 	},
+}
+
+func OutFileCopy(flag *OutfileCmdFlag, env *Env) error {
+
+	dir, err := os.Getwd()
+	if err != nil {
+		return errors.Wrap(err, "get working directory failed:%s")
+	}
+
+	gArg := &generate.GeneratorArg{
+		Action:         common.Outfile,
+		GoFile:         dir + "/" + env.GoFile,
+		GoLine:         env.GoLine,
+		GoPkg:          env.GoPackage,
+		OutFile:        dir + "/" + env.GoFile,
+		OutLine:        env.GoLine,
+		SrcName:        "src",
+		SrcType:        flag.SrcType,
+		SrcPkg:         flag.SrcPkg,
+		DstName:        "dst",
+		DstPkg:         flag.DstPkg,
+		DstType:        flag.DstType,
+		LoadConfigOpts: nil,
+		Print:          false,
+	}
+	err = generateCode(gArg)
+	if err != nil {
+		return errors.Wrap(err, "generate failed")
+	}
+	return nil
 }
